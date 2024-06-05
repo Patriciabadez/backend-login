fa40c97ff15a53ba9f3defc2eb079f5e610defb7
Patricia Baldez <patriciabaldez1985@gmail.com>
2024-06-05 10:45:32 -0300

    conexão nodejs e mysql

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ const express = require("express");
 const app = express();
 const User = require('./models/User');
 
+app.use(express.json());
+
 app.get('/', async (req, res) => {
     res.send("Pagina Inicial!");
 });
-app.post('/cadastrar',async(req, res) => {
-    console.log(req.body)
-res.send("Pagina cadastrar")
-})
+app.post('/cadastrar', async (req, res) => {
+    //console.log(req.body);
+
+    await User.create(req.body)
+        .then(() => {
+            return res.status(200).json({
+                erro: false,
+                mensagem: "Usuário cadastrado com sucesso!"
+            })
+        }).catch(() => {
+            return res.status(400).json({
+                erro: true,
+                mensagem: "Erro: Usuário não cadastrado!"
+
+            })
+
+            //res.send("Pagina cadastrar");
+        });
+});
 
 app.listen(3001, () => {
     console.log("Servidor rodando na porta 3001: http://localhost:3001");
